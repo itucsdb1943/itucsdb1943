2ee2e6dce054031be2b5897b9ecba1239d1dbd46
ilknurcelik <ilknurcelik_99@hotmail.com>
2019-11-26 23:17:22 +0300

    The classes of the Foundation and Blog pages are added

---
+++ classes/blog.py
@@ -0,0 +1,10 @@
+class Blog:
+	def __init__(self, blogID, userID, tag, title, text, likeNum, dislikeNum, postedDate):
+		self.blogID = blogID
+		self.userID = userID
+		self.tag = tag 
+		self.title = title
+		self.text = text
+		self.likeNum = likeNum
+		self.dislikeNum = dislikeNum
+		self.postedDate = postedDate
---
+++ classes/foundation.py
@@ -0,0 +1,11 @@
+class Foundation:
+    def __init__(self, foundID, photo, donationURL, facebookLink, twitterLink, youtubeLink, instagramLink, about, name):
+        self.foundID = foundID
+        self.photo = photo
+        self.donationURL = donationURL
+        self.facebookLink = facebookLink
+        self.twitterLink = twitterLink
+        self.youtubeLink = youtubeLink
+        self.instagramLink = instagramLink
+        self.about = about
+        self.name = name
